ba60b814ffe72b569473dd9cc87f1a630964b44d
Orlando Arzola <>
2025-05-08 14:28:29 +0200

    add pods

---
+++ Pods/Target Support Files/Pods-WallaMarvel-WallaMarvelUITests/Pods-WallaMarvel-WallaMarvelUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WallaMarvel_WallaMarvelUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WallaMarvel_WallaMarvelUITests
+@end
---
+++ Pods/Target Support Files/Pods-WallaMarvelTests/Pods-WallaMarvelTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WallaMarvelTests : NSObject
+@end
+@implementation PodsDummy_Pods_WallaMarvelTests
+@end
